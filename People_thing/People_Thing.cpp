4f87cdef29f480dbc4b42638d6e975175f421928
IsaacSimmons96 <47719635+IsaacSimmons96@users.noreply.github.com>
2019-02-18 20:52:11 +0000

    Add files via upload

---
+++ People_thing/People_Thing.cpp
@@ -0,0 +1,118 @@
+// People_Thing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "../People_Thing/Headers/person.h"
+#include "../People_Thing/Headers/time.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <list>
+#include <vector>
+#include <time.h>       
+
+using namespace std;
+
+Date* world_time = new Date();
+
+Person* generate_random_person(vector<string> m_names, vector<string> f_names, vector<string> s_names)
+{
+
+	int temp = rand() % 2;
+
+	if (temp == 0)
+	{
+		Person* person = new Person(m_names[rand() % m_names.size()], s_names[rand() % s_names.size()], Male, static_cast<Month>(rand() % December + January), rand() % 99 + 1920);
+		return person;
+	}
+	else if (temp == 1)
+	{
+		Person* person = new Person(f_names[rand() % f_names.size()], s_names[rand() % s_names.size()], Female, static_cast<Month>(rand() % December + January), rand() % 99 + 1920);
+		return person;
+	}
+
+	else
+	{
+		cout << "ERROR GENDER IS NOT BOY OR GIRL" << endl;
+		cout << "TEMP = " << temp << endl;
+
+	}
+}
+
+
+int main()
+{
+	ifstream Stream1("Names.txt");
+	if (!Stream1.is_open()) {
+		cout << "Failed to open file" << endl;
+		return 0;
+	}
+
+	// LIST CONTAINS 61797 ENTRIES
+	vector<string> Mnames, Fnames, Snames;
+	string yr, sex, FirstForename, number, rank, position, Surname;
+	string line;
+
+	while (getline(Stream1, line)) {
+		stringstream ss(line);
+		getline(ss, yr, ',');
+		getline(ss, sex, ',');
+		getline(ss, FirstForename, ',');
+
+		if (sex == "B")
+		{
+			Mnames.push_back(FirstForename);
+		}
+		else
+		{
+			Fnames.push_back(FirstForename);
+		}
+
+	}
+	Stream1.close();
+
+	ifstream Stream2("Surnames.txt");
+	if (!Stream2.is_open()) {
+		cout << "Failed to open file" << endl;
+		return 0;
+	}
+
+	while (getline(Stream2, line)) {
+		stringstream ss(line);
+		getline(ss, Surname, ',');
+		Snames.push_back(Surname);
+
+	}
+	Stream2.close();
+
+	list<Person*> People;
+
+	srand(time(0));
+
+	for (int i = 0; i != 100; i++)
+	{
+		People.push_back(generate_random_person(Mnames, Fnames, Snames));
+	}
+
+
+
+	Person* Isaac = new Person("Isaac", "Simmons", Male, April, 1996, 26);
+	Person* Terri = new Person("Terri", "Gardner", Female, January, 1996, 14);
+	People.push_back(Isaac);
+	People.push_back(Terri);
+
+	int i = 1;
+	for (list<Person*>::iterator it = People.begin(); it != People.end(); it++)
+	{
+		(*it)->print_info(i);
+		cout << " AGE:   " + to_string((*it)->get_age(world_time->get_day(), world_time->get_month(), world_time->get_year())) << endl;
+		i++;
+	}
+
+	cout << endl << endl << "TODAY'S DATE: " << world_time->get_date() << endl;
+
+
+	return 0;
+}
+
