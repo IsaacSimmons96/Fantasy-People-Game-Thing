750bef930ff13b12c3d11679e41fd6510988deef
isaacsimmons96 <isaacsimmons96@yahoo.co.uk>
2019-02-19 22:55:58 +0000

    Fixed Arrays & Timers Changed Vectors to Fixed Arrays - this increased speed of people generation from 16seconds to < 0 seconds Added seconds timers to debuging text.

--- People_thing/People_Thing.cpp
+++ People_thing/People_Thing.cpp
@@ -10,26 +10,29 @@
 #include <string>
 #include <list>
 #include <vector>
-#include <time.h>       
+#include <time.h>     
+#include <utility>
 
 using namespace std;
 
 Date* world_time = new Date();
 
-Person* generate_random_person(vector<string> m_names, vector<string> f_names, vector<string> s_names)
+Person* generate_random_person(string m_names[], string f_names[],string s_names[])
 {
 
 	int temp = rand() % 2;
 
 	if (temp == 0)
 	{
-		Person* person = new Person(m_names[rand() % m_names.size()], s_names[rand() % s_names.size()], Male, Ethnicity(rand() % INVALID), static_cast<Month>(rand() % December + January), rand() % 99 + 1920);
+		Person* person = new Person(m_names[rand() % 26756], s_names[rand() % 14675], Male, Ethnicity(rand() % INVALID), static_cast<Month>(rand() % December + January), rand() % 99 + 1920);
 		return person;
+		delete(person);
 	}
 	else if (temp == 1)
 	{
-		Person* person = new Person(f_names[rand() % f_names.size()], s_names[rand() % s_names.size()], Female, Ethnicity(rand() % INVALID), static_cast<Month>(rand() % December + January), rand() % 99 + 1920);
+		Person* person = new Person(f_names[rand() % 35041], s_names[rand() % 14675], Female, Ethnicity(rand() % INVALID), static_cast<Month>(rand() % December + January), rand() % 99 + 1920);
 		return person;
+		delete(person);
 	}
 
 	else
@@ -43,6 +46,9 @@ Person* generate_random_person(vector<string> m_names, vector<string> f_names, v
 
 int main()
 {
+	time_t start;
+	time(&start);
+	double seconds, total_seconds;
 	ifstream Stream1("Names_New.txt");
 	if (!Stream1.is_open()) {
 		cout << "Failed to open file" << endl;
@@ -50,10 +56,13 @@ int main()
 	}
 
 	// LIST CONTAINS 61797 ENTRIES
-	vector<string> Mnames, Fnames, Snames;
+	string* Mnames = new string[26756];
+	string* Fnames = new string[35041];
+	string* Snames = new string[14675];
 	string sex, FirstForename, Surname;
 	string line;
 
+	int fname_counter = 0, mname_counter = 0;
 	while (getline(Stream1, line)) {
 		stringstream ss(line);
 		getline(ss, sex, ',');
@@ -61,11 +70,13 @@ int main()
 
 		if (sex == "B")
 		{
-			Mnames.push_back(FirstForename);
+			Mnames[mname_counter] = FirstForename;
+			mname_counter++;
 		}
 		else
 		{
-			Fnames.push_back(FirstForename);
+			Fnames[fname_counter] = FirstForename;
+			fname_counter++;
 		}
 
 	}
@@ -78,26 +89,34 @@ int main()
 		return 0;
 	}
 
+
+	mname_counter = 0;
 	while (getline(Stream2, line)) {
 		stringstream ss(line);
 		getline(ss, Surname, ',');
-		Snames.push_back(Surname);
+		Snames[mname_counter] = Surname;
+		mname_counter++;
 
 	}
 	Stream2.close();
 	cout << "Finished Surnames CSV" << endl << endl;
 
+
 	list<Person*> People;
 
 	srand(time(0));
+
+	time_t now;
+	time(&now);
 
 	cout << "Starting People Generation" << endl << endl;
 	for (int i = 0; i != 100; i++)
 	{
 		People.push_back(generate_random_person(Mnames, Fnames, Snames));
 	}
-	cout << "Finished People Generation" << endl << endl;
 
+
+	seconds = difftime(time(0), now);
 
 	Person* Isaac = new Person("Isaac", "Simmons", Male, White, April, 1996, 26);
 	Person* Terri = new Person("Terri", "Gardner", Female, White, January, 1996, 14);
@@ -109,12 +128,15 @@ int main()
 	for (list<Person*>::iterator it = People.begin(); it != People.end(); it++)
 	{
 		(*it)->print_info(i);
-		cout << " AGE:   " + to_string((*it)->get_age(world_time->get_day(), world_time->get_month(), world_time->get_year())) << endl;
+		cout << " AGE    : " + to_string((*it)->get_age(world_time->get_day(), world_time->get_month(), world_time->get_year())) << endl;
 		i++;
 	}
 
-	cout << endl << endl << "TODAY'S DATE: " << world_time->get_date() << endl;
+	total_seconds = difftime(time(0), start);
 
+	cout << endl << endl << "TODAY'S DATE: " << world_time->get_date() << endl;
+	cout << "Finished People Generation in " << seconds << " seconds" << endl;
+	cout << "Finished Program in " << total_seconds << " seconds " << endl << endl;
 
 	return 0;
 }
